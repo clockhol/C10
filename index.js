1abf33ae14920f3eda988fbc0d37a45d1b1231f1
clockhol <clockh05@yahoo.com>
2025-04-17 21:22:06 +0000

    CSS

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const titleContainer = document.getElementById('placeholder-title');
  fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
     .then(json => titleContainer.textContent = json.title);
+
+    const titleContainer = document.getElementById('placeholder-body');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+       .then(response => response.json())
+       .then(json => titleContainer.textContent = json.body);
